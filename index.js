9556023d9e2dca5dac48495a6588e08a2f84cc43
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2019-07-09 14:05:16 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('works')
+
+$(function() {
+    $.scrollify({
+      section : ".sec",
+    });
+  });
+
+
+  $.scrollify({
+section : "section",
+sectionName : "section-name",
+interstitialSection : "",
+easing: "easeOutExpo",
+scrollSpeed: 1100,
+offset : 0,
+scrollbars: true,
+standardScrollElements: "",
+setHeights: true,
+overflowScroll: true,
+updateHash: true,
+touchScroll:true,
+before:function() {},
+after:function() {},
+afterResize:function() {},
+afterRender:function() {}
+});
+
+
+
